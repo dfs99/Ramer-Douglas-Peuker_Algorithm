74beb8fcbb2b5e252033ffb8b0c0261bd834796c
diegoFS <100387203@alumnos.uc3m.es>
2021-04-08 16:47:48 +0200

    FIRST COMMIT, SETTING THE FILES

---
+++ fetch_data.py
@@ -0,0 +1,11 @@
+"""
+    first line: max_error and number of points.
+    the following lines will contain the points as a two value.
+    points will be float.
+
+    for example:
+    0.4 2
+    1.0 3.5
+    -2.4 1.7
+
+"""
---
+++ plot_data.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [1, -3, 3]
+y = [2, -1, 4]
+
+data = [(1, 2), (5, 7), (-1, 5)]
+
+plt.plot(x, y, "-o")
+plt.show()
+
