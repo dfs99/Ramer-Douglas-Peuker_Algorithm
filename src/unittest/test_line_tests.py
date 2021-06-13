69a15d68a63f7d20c3bfcddd5ce53ce7f345cbd1
DiegoFS <100387203@alumnos.uc3m.es>
2021-06-13 20:02:57 +0200

    requirements.txt & tests

---
+++ src/unittest/test_line_tests.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+
+class TestStraightLine(TestCase):
+
+    def setUpClass(cls) -> None:
+        pass
+
+    def tearDownClass(cls) -> None:
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
