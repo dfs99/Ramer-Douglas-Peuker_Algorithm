8be0a99ba716f0e4a63527204475c60ac510e929
diegoFS <100387203@alumnos.uc3m.es>
2021-08-22 13:20:25 +0200

    added testing images + scatter checker.py

--- src/data/ficheros_para_eliminar/dibujar_res_cpp.py
+++ src/data/ficheros_para_eliminar/dibujar_res_cpp.py
@@ -8,6 +8,6 @@ epsilon_error, data = current_parser.extract_data()
 STARTING_POINTS = list(data)
 FIGURE = plt.figure()
 AXES = FIGURE.add_subplot()
-AXES.plot([point.x for point in STARTING_POINTS], [point.y for point in STARTING_POINTS], color='k', linestyle='dashed',
-         marker='o', lw=1)
+AXES.scatter([point.x for point in STARTING_POINTS], [point.y for point in STARTING_POINTS], color='k',
+         marker='x')
 plt.show()
