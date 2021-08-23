ae8a2728e6d340ef8d23c3055d79493ab67f1373
dfs99 <100387203@alumnos.uc3m.es>
2021-08-23 16:21:58 +0200

    Chrono for measuring performance.

--- src/files_management/cpp_srcs/srcs/bmp_parser.cpp
+++ src/files_management/cpp_srcs/srcs/bmp_parser.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <chrono>
 #include "../headers/BMPfile.h"
 
 using namespace std;
 int main(){
-    const string filename = "../images/large.bmp";
-    BMPfile my_file {filename};
-    my_file.generate_point_file();
-    my_file.print_values(1);
-    my_file.generate_bmp_file();
+    const string filename = "../images/spain_map.bmp";
+    auto start = std::chrono::system_clock::now();
+    try{
+        BMPfile my_file {filename};
+        my_file.generate_point_file();
+        //my_file.print_values(1);
+        my_file.generate_bmp_file();
+    }catch (const exception & e) {
+        cerr << e.what() << endl;
+        return -1;
+    }
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double>duration = end - start;
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+    cout << "ELapsed time:\t" << milliseconds << " ms " << endl;
+    return 0;
 }
